92ad2c1b8912f1d80bc8eb92972a502719db9092
Diego Andrés Regino Penagos <diegoregino1994@gmail.com>
2022-12-21 16:44:46 -0500

    add icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,25 +12,24 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+        <meta name="description" content="Experiencias en Colombia" />
+        <meta name="keywords" content="viajes, experciencias, vida, aventura" />
+        <title>Wonder travel</title>
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/icon.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/icon-2x.png"
           rel="icon"
           type="image/png"
           sizes="32x32"
         />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <meta name="theme-color" content="#2D3136" />
       </Head>
       <Component {...pageProps} />
     </>
